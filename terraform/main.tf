d5dae1081f791e4b57451e6dc0ea40f9267b1c53
SamirPawar98 <samirppawar@gmail.com>
2025-08-08 11:34:58 +0530

    updated teraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,56 +1,3 @@
-# provider "aws" {
-#   region = var.region
-# }
-
-# resource "aws_glue_job" "my_glue_job" {
-#   name     = "my-glue-job"
-#   role_arn = var.existing_glue_role_arn
-
-#   command {
-#     name            = "glueetl"
-#     script_location = var.glue_script_s3_path
-#     python_version  = "3"
-#   }
-
-#   glue_version = "4.0"
-#   max_capacity = 2
-# }
-
-# resource "aws_glue_crawler" "my_crawler" {
-#   name         = "my-glue-crawler"
-#   role         = var.existing_glue_role_arn
-#   database_name = "lottery-db"
-
-#   s3_target {
-#     path = "s3://your-bucket/lottery-data/"
-#   }
-
-#   schedule = "cron(0 12 * * ? *)"
-# }
-
-# data "archive_file" "lambda_zip" {
-#   type        = "zip"
-#   source_file = "${path.module}/lambda_function.py"
-#   output_path = "${path.module}/lambda_function.zip"
-# }
-
-# resource "aws_lambda_function" "glue_trigger_lambda" {
-#   function_name = "trigger-glue-job"
-#   role          = var.existing_lambda_role_arn
-#   handler       = "lambda_function.lambda_handler"
-#   runtime       = "python3.10"
-#   timeout       = 30
-
-#   filename         = data.archive_file.lambda_zip.output_path
-#   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-
-#   environment {
-#     variables = {
-#       GLUE_JOB_NAME = aws_glue_job.my_glue_job.name
-#     }
-#   }
-# }
-
 # Configure the AWS provider with the specified region
 provider "aws" {
   region = var.aws_region
@@ -68,6 +15,13 @@ resource "aws_glue_job" "transform_job" {
     name            = "glueetl"                        # Job type (glueetl = Spark ETL)
     script_location = var.glue_script_s3_path          # S3 path to transformation script
     python_version  = "3"                              # Use Python 3
+  }
+
+  default_arguments = {
+    "--TempDir"               = "s3://my-temp-bucket/glue-temp/"
+    "--job-bookmark-option"   = "job-bookmark-disable"
+    "--enable-metrics"        = "true"
+    "--enable-continuous-cloudwatch-log" = "true"
   }
 
   # Retry and timeout configurations
@@ -98,37 +52,6 @@ resource "aws_glue_crawler" "transformed_data_crawler" {
     path = var.transformed_data_s3_path                # S3 path to transformed data
   }
 
-  # Optional: Crawler output behavior
-  configuration = jsonencode({
-    Version = 1.0,
-    CrawlerOutput = {
-      Partitions = { AddOrUpdateBehavior = "InheritFromTable" }
-    }
-  })
+  depends_on = [aws_glue_job.transform_job]
 
-  
 }
-
-###############################################
-# Trigger Resource (Runs Glue Job and Crawler)
-###############################################
-resource "null_resource" "trigger_glue_flow" {
-  # Ensure dependencies are created before running the commands
-  depends_on = [
-    aws_glue_job.transform_job,
-    aws_glue_crawler.transformed_data_crawler
-  ]
-
-  # Run the job and crawler using local AWS CLI
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "Starting Glue Job..." && \
-      aws glue start-job-run --job-name lottery-transform-job && \
-      echo "Waiting for job to complete..." && \
-      sleep 120 && \
-      echo "Starting Transformed Data Crawler..." && \
-      aws glue start-crawler --name lottery-transformed-crawler
-    EOT
-  }
-}
-
